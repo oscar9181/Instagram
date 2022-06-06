23d27edb0aad77a7e4ad3ceca98dd30e462689ff
oscar9181 <oscarokola17@gmail.com>
2022-06-06 12:28:49 +0300

    add logout functionality

--- gram/views.py
+++ gram/views.py
@@ -1,9 +1,19 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from.models import Post
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate,login,logout
+
+from django.contrib import messages
+
 from.forms import CreateUserForm
+
+
+
+def insta(request):
+    return render(request,'instagram/insta.html')
+
 
 
 def registerPage(request):
@@ -13,16 +23,33 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-        
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for' + user)
+            
+            return redirect('login')
         
     context = {'form':form}
     return render(request,'instagram/register.html',context)
 
 def loginPage(request):
+   
+    
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('instagram')
+        else:
+           messages.info(request,'Username OR Password is incorrect')
+       
     context = {}
     return render(request,'instagram/login.html',context)
 
 
-
-def insta(request):
-    return render(request,'instagram/insta.html')
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
