97c1c9d26d652a9bed9c6c350826861f8d8f9b30
oscar9181 <oscarokola17@gmail.com>
2022-06-06 06:19:22 +0300

    login and register path

--- gram/urls.py
+++ gram/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/',views.registerPage,name='register'),
+    path('login/',views.loginPage,name='login'),
+    
     path('',views.insta,name='instagram')
 ]
--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
 from.models import Post
 
-# Create your views here.
+
+def registerPage(request):
+    context = {}
+    return render(request,'instagram/register.html',context)
+
+def loginPage(request):
+    context = {}
+    return render(request,'instagram/login.html',context)
+
+
 
 def insta(request):
     return render(request,'instagram/insta.html')
